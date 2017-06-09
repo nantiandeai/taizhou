60c2912a11de408d4d9bdc5f63a85455ba231f74
caiyong <893468633@qq.com>
2017-06-09 14:16:47 +0800

    分馆管理

---
+++ src/main/java/com/creatoo/hn/services/home/userCenter/BlackListService.java
@@ -0,0 +1,17 @@
+package com.creatoo.hn.services.home.userCenter;
+
+import org.apache.log4j.Logger;
+
+/**黑名单服务
+ * Created by caiyong on 2017/6/9.
+ */
+public class BlackListService {
+
+    private static Logger logger = Logger.getLogger(BlackListService.class);
+
+
+
+    public void doTask(){
+
+    }
+}
---
+++ src/main/java/com/creatoo/hn/task/StartUpListener.java
@@ -0,0 +1,55 @@
+package com.creatoo.hn.task;
+
+import com.creatoo.hn.services.home.userCenter.BlackListService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**启动项监听器
+ * Created by caiyong on 2017/6/9.
+ */
+@Component("StartUpListener")
+public class StartUpListener implements ApplicationContextAware {
+
+    private static Logger logger = Logger.getLogger(StartUpListener.class);
+
+    private static final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+    private ApplicationContext applicationContext;
+
+    private BlackListService blackListService;
+
+    public void setBlackListService(BlackListService blackListService) {
+        this.blackListService = blackListService;
+    }
+
+    public BlackListService getBlackListService() {
+        return blackListService;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        scheduledExecutorService.scheduleAtFixedRate(myRunner, 3, 3, TimeUnit.SECONDS);
+    }
+
+    private Runnable myRunner = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                if(null != blackListService){
+                    blackListService.doTask();
+                }
+            }catch (Exception e){
+                logger.error(e.toString());
+                scheduledExecutorService.shutdown();
+            }
+        }
+    };
+}
