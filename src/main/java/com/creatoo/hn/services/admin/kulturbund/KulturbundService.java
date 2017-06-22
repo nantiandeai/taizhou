c342bc3e2776007ec44d8a118bcbeb6adacc2103
caiyong <893468633@qq.com>
2017-06-22 12:02:08 +0800

    分馆管理b
    Signed-off-by: caiyong <893468633@qq.com>

---
+++ src/main/java/com/creatoo/hn/services/admin/kulturbund/KulturbundService.java
@@ -0,0 +1,17 @@
+package com.creatoo.hn.services.admin.kulturbund;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+/**文化联盟服务
+ * Created by caiyong on 2017/6/22.
+ */
+@Service
+@SuppressWarnings("all")
+public class KulturbundService {
+
+    private static Logger logger = Logger.getLogger(KulturbundService.class);
+
+    
+
+}
