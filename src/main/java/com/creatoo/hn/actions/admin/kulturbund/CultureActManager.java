4bb6f03ca77da8b14c82b932d60691813812d399
caiyong <893468633@qq.com>
2017-06-26 15:15:42 +0800

    分馆管理b
    Signed-off-by: caiyong <893468633@qq.com>

--- src/main/java/com/creatoo/hn/actions/admin/kulturbund/CultureActManager.java
+++ src/main/java/com/creatoo/hn/actions/admin/kulturbund/CultureActManager.java
@@ -1,7 +1,48 @@
 package com.creatoo.hn.actions.admin.kulturbund;
 
-/**
- * Created by Administrator on 2017/6/26.
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**文化联盟大型活动控制器
+ * Created by caiyong on 2017/6/26.
  */
+@SuppressWarnings("all")
+@RestController
+@RequestMapping("/admin/cultureact")
 public class CultureActManager {
+    private static Logger logger = Logger.getLogger(CultureActManager.class);
+
+    /**
+     * 列表页面入口
+     * @param request
+     * @param type
+     * @return
+     */
+    @RequestMapping("/list/{type}")
+    public ModelAndView actList(HttpServletRequest request, @PathVariable("type") String type){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("listType",type);
+        modelAndView.setViewName("admin/kulturbund/cultureact/view_list");
+        return modelAndView;
+    }
+
+    /**
+     * 获取参数
+     * @param request
+     * @param paramName
+     * @param defaultValue
+     * @return
+     */
+    private String getParam(HttpServletRequest request,String paramName,String defaultValue){
+        String value = request.getParameter(paramName);
+        if(null == value || value.trim().isEmpty()){
+            return defaultValue;
+        }
+        return value;
+    }
 }
